b63038b1087de5c1f4254aad440526880c28df6b
Indian1999 <haszab99@gmail.com>
2025-11-13 17:44:37 +0100

    pendulum

--- sajat_modul.py
+++ sajat_modul.py
@@ -2,11 +2,29 @@ import LogiLib # Lefuttatja a LogiLib mappában lévő __init__.py-t
 from LogiLib import constants # (Ez akkor is működik, ha az init.py-ban nincs import)
 from LogiLib.constants import nevek
 from LogiLib.funcs import fib
+import datetime
+import time
+import pendulum as p # pip install pendulum
 
-print(LogiLib.constants.golden_ratio)
-print(constants.pi)
-print(nevek)
+print(datetime.date.today()) # 2025-11-13
 
-print(LogiLib.funcs.factorial(8))
+today = datetime.date.today()
+print(today.weekday()) # 3 (csütörtök)
+print(today.day)
+print(today.month)
+print(today.year)
+print(today.strftime("Év: %Y, Hónap: %m, nap: %d\n%H:%M:%S"))
 
-print(fib(11))
+this_moment = datetime.datetime.now()
+print(this_moment.strftime("Év: %Y, Hónap: %m, nap: %d\n%H:%M:%S"))
+
+print(time.time()) # 1763051472.362606
+print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+city = "Australia/Sydney"
+city_now = p.now(city)
+print(city_now.to_datetime_string())
+
+duriation = p.duration(seconds=time.time())
+print(duriation.in_days())
+
