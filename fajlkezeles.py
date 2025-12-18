d5103076b34aa56f8d25e3d0d096d4c118d14c75
Indian1999 <haszab99@gmail.com>
2025-12-18 16:45:12 +0100

    pontok.txt

---
+++ fajlkezeles.py
@@ -0,0 +1,22 @@
+import os
+
+file = open("story.txt", "r", encoding="utf-8")
+#szöveg = file.read() # egész szöveget beolvassa
+print(file.read(10)) # 10 KARAKTERT beolvas
+print(file.read(20))
+file.seek(0) # Ez olvasófejet a 0. karakterre helyezi
+print(file.read(11))
+file.close()
+
+# Context Manager (with), a fájl csak a with-en belül érhető el
+with open("story.txt", "r", encoding="utf-8") as f:
+    data = f.readlines() # ["sor1\n", "sor2\n", "sor3\n", ...]
+
+with open("homerseklet.txt", "r", encoding="utf-8") as f:
+    homersekletek = []
+    for line in f:
+        line = line.strip() # Leszedi a whitespace karaktereket az elejéről és a végéről
+        line = line.split(";") # ['13', '15', '11', '29', '24', '27', '10']
+        szamok = [int(item) for item in line]
+        print(szamok) # [14, 33, -3, -5, -5, 9, 14]
+        homersekletek.append(szamok)
